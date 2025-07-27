aad7d4dd711bde4263be909a62633602daf5042f
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2025-07-27 10:26:10 +0700

    fix: title is optional

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from "astro/loaders";
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/pages/posts/" }),
   schema: z.object({
-    title: z.string(),
+    title: z.string().optional(),
     publishDate: z.string(),
     modifiedDate: z.string().optional(),
     topic: z.string(),
